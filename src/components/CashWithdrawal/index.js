81f6a279a40198f1756e1d465ee136e935d59a11
khyathiPriya6 <116505157+khyathiPriya6@users.noreply.github.com>
2022-12-15 21:05:07 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -1,49 +1,52 @@
-// Write your code here
 import {Component} from 'react'
 
-import './index.css'
 import DenominationItem from '../DenominationItem'
 
-class CashWithdrawal extends Component {
-  state = {balance: 2000}
+import './index.css'
 
-  decreaseAmount = value => {
+class CashWithdrawal extends Component {
+  state = {
+    balance: 2000,
+  }
+
+  updateBalance = value => {
     this.setState(prevState => ({balance: prevState.balance - value}))
   }
 
   render() {
     const {denominationsList} = this.props
     const {balance} = this.state
+    const name = 'Sarah Williams'
+    const initial = name.slice(0, 1)
 
     return (
-      <div className="bg-container">
-        <div className="main-container">
-          <div className="profile-section">
-            <div className="profile-pic">
-              <p className="profile-pic-name">S</p>
+      <div className="app-container">
+        <div className="cash-withdrawal-container">
+          <div className="user-details-container">
+            <div className="initial-container">
+              <p className="initial">{initial}</p>
             </div>
-            <p className="profile-name">Sarah Williams</p>
+            <p className="name">{name}</p>
           </div>
           <div className="balance-container">
-            <p className="balance-heading">Your Balance</p>
-            <div className="amount-container">
-              <p className="amount">{balance}</p>
-              <p className="rupee-para">in Rupees</p>
-            </div>
+            <p className="your-balance">Your Balance</p>
+            <p className="balance">
+              {balance}
+              <br />
+              <span className="currency">In Rupees</span>
+            </p>
           </div>
-          <div className="withdraw-section">
-            <p className="withdraw-para">Withdraw</p>
-            <p className="choose-amount">CHOOSE SUM (IN RUPEES)</p>
-            <ul className="amount-btn-container">
-              {denominationsList.map(eachItem => (
-                <DenominationItem
-                  denominationAmount={eachItem}
-                  key={eachItem.id}
-                  onClickingAmount={this.decreaseAmount}
-                />
-              ))}
-            </ul>
-          </div>
+          <p className="withdraw">Withdraw</p>
+          <p className="choose-sum">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="denominations-list">
+            {denominationsList.map(eachDenomination => (
+              <DenominationItem
+                key={eachDenomination.id}
+                denominationDetails={eachDenomination}
+                updateBalance={this.updateBalance}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     )
